079ee838671da5c79da264807b0a3ef20bcd1ef7
JiaSin Liu <joeyliu7975@gmail.com>
2023-02-24 19:56:22 +0800

    [feat]: Prevent user select unavailable year of WWDC

--- Sources/WWDCCrawler/WWDCCrawler.swift
+++ Sources/WWDCCrawler/WWDCCrawler.swift
@@ -18,14 +18,22 @@ public struct WWDCCrawler {
                 print("Passing parameter \(CommandLine.arguments[1]) is not valid year, please try again!")
             }
         }
+        // TODO: Replace `2013` by obtainning the earliest year of WWDC, retrieve data from https://developer.apple.com/videos/all-videos/.
+        guard year > 2013, year < getCurrentYear() - 1 else {
+            print("The selecting year of WWDC videos are not available from https://developer.apple.com/videos/all-videos/")
+            return
+        }
         
         let sema = DispatchSemaphore(value: 0)
+        
         let url = URLFactory.makeURL(scheme: .https,
                                      host: .appleDeveloper,
                                      path: "/videos/wwdc\(year)/")
+        
         let client = URLSessionClient()
         let parser = SwiftSoupHTMLParser()
         let crawler = WebCrawler(url: url, client: client, parser: parser)
+        
         crawler.crawlingURL { [sema] (result) in
             switch result {
             case let .success(conference):
