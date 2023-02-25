0d575f9b6c1466632e0f6ce9d3bd92e8f3b514aa
JiaSin Liu <joeyliu7975@gmail.com>
2023-02-25 09:11:39 +0800

    [refactor]: Extract `FilterContentDocument`, `FilterVideoTitles` and `FilterVideoLinks`

--- Sources/WWDCCrawler/Parser/SwiftSoupParser.swift
+++ Sources/WWDCCrawler/Parser/SwiftSoupParser.swift
@@ -12,29 +12,11 @@ final class SwiftSoupHTMLParser: HTMLParser {
     func parse(htmlString: String) throws -> Conference {
         do {
             let doc = try SwiftSoup.parse(htmlString)
+            let contentDoc = try filterContentDocument(doc)
             
             let title = try doc.title()
-            
-            let allContents = try doc.select("section").attr("class", "all-content").html()
-            
-            let contentDoc = try SwiftSoup.parse(allContents)
-            
-            let videoTitles = try contentDoc.select("h4")
-                .attr("class", "video-title")
-                .compactMap({ try? $0.html() })
-                .reduce(into: [String](), { result, e in
-                    if !result.contains(e) {
-                        result.append(e)
-                    }
-                })
-            
-            let links = try contentDoc.select("a")
-                .compactMap({ try? $0.attr("href")})
-                .reduce(into: [String](), { result, e in
-                    if e.contains("wwdc"), !result.contains(e) {
-                        result.append(e)
-                    }
-                })
+            let videoTitles = try filerVideoTitles(doc: contentDoc)
+            let links = try filterLinks(doc: contentDoc)
             
             let videos = zip(videoTitles, links).map {
                 let url = URLFactory.makeURL(scheme: .https,
@@ -45,6 +27,48 @@ final class SwiftSoupHTMLParser: HTMLParser {
             }
             
             return Conference(title: title, videos: videos)
+        } catch {
+            throw error
+        }
+    }
+    
+    private func filterContentDocument(_ doc: Document) throws -> Document {
+        do {
+            let allContents = try doc.select("section").attr("class", "all-content").html()
+            let contentDoc = try SwiftSoup.parse(allContents)
+            return contentDoc
+        } catch {
+            throw error
+        }
+    }
+    
+    private func filterLinks(doc: Document) throws -> [String] {
+        do {
+            let links = try doc.select("a")
+                .compactMap({ try? $0.attr("href")})
+                .reduce(into: [String](), { result, e in
+                    if e.contains("wwdc"), !result.contains(e) {
+                        result.append(e)
+                    }
+                })
+            return links
+        } catch {
+            throw error
+        }
+    }
+    
+    private func filerVideoTitles(doc: Document) throws -> [String] {
+        do {
+            let titles = try doc.select("h4")
+                .attr("class", "video-title")
+                .compactMap({ try? $0.html() })
+                .reduce(into: [String](), { result, e in
+                    if !result.contains(e) {
+                        result.append(e)
+                    }
+                })
+            
+            return titles
         } catch {
             throw error
         }
